3b31b78f12c75f46dde32762a35da7e7b7da09a9
uobikiemukot <uobikiemukot@gmail.com>
2014-07-31 07:56:29 +0900

    always use ACTIVE_CURSOR_COLOR

--- pseudo.h
+++ pseudo.h
@@ -74,7 +74,8 @@ static inline void draw_line(struct pseudobuffer *pb, struct terminal *term, int
 			|| (cellp->width == WIDE && (col + 1) == term->cursor.x)
 			|| (cellp->width == NEXT_TO_WIDE && (col - 1) == term->cursor.x))) {
 			color_pair.fg = DEFAULT_BG;
-			color_pair.bg = (!tty.visible && BACKGROUND_DRAW) ? PASSIVE_CURSOR_COLOR: ACTIVE_CURSOR_COLOR;
+			//color_pair.bg = (!tty.visible && BACKGROUND_DRAW) ? PASSIVE_CURSOR_COLOR: ACTIVE_CURSOR_COLOR;
+			color_pair.bg = ACTIVE_CURSOR_COLOR;
 		}
 
 		for (h = 0; h < CELL_HEIGHT; h++) {
